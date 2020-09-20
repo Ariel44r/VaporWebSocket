01de9ec564812a7f63bb7e2258300f1e0fb50555
ARIEL RAMIREZ <ariel44r@gmail.com>
2020-09-20 03:46:51 -0500

    [Ariel´s log]: begin to improve fastlane CI

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -12,4 +12,16 @@ final class AppTests: XCTestCase {
             XCTAssertEqual(res.body.string, "Hello, world!")
         }
     }
+    
+    func testFeature0() throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try configure(app)
+
+        try app.test(.GET, "feature0") { res in
+            debugPrint(res.status)
+            XCTAssertEqual(res.status, .ok)
+//            XCTAssertEqual(res.body.string, "Hello, world!")
+        }
+    }
 }
